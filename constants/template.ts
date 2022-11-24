777f898682adbad49233bf66575a21f00437976a
amine-abdelli <amine.abdelli@outlook.fr>
2022-11-24 15:10:31 +0100

    feat(template-string): Migrate template string to constants

---
+++ constants/template.ts
@@ -0,0 +1,6 @@
+const ComponentNameTemplate = '<%component_name%>';
+const ComponentExportTemplate = '<%component_export%>';
+const ComponentImportTemplate = '<%component_import%>';
+const StyleImportTemplate = '<%style_import%>';
+
+export { ComponentNameTemplate, ComponentExportTemplate, ComponentImportTemplate, StyleImportTemplate }
