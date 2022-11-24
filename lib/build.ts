cbed7a837d9d1c5c197b93328ab93136e88c573d
amine-abdelli <amine.abdelli@outlook.fr>
2022-11-24 15:54:55 +0100

    refacto(build.ts): Refacto build.ts

--- lib/build.ts
+++ lib/build.ts
@@ -1,60 +1,59 @@
-import fs, { writeFile } from 'fs';
-import { ComponentExportTemplate, ComponentImportTemplate, ComponentNameTemplate, PathPolicyRegex, StyleImportTemplate } from '../constants';
-import { sanitizeConfigPaths, sanitizePath, createDirIfNotExist, buildFile } from '../utils';
-// Possibility to pass name in config "name": "Button"
-// Propose when create file to prefill them or not
-// Handle case user pick 'none' suffix
+import fs from 'fs';
+import { ComponentExportTemplate, ComponentImportTemplate, ComponentNameTemplate, StyleImportTemplate } from '../constants';
+import { sanitizeConfigPaths, sanitizePath, createDirIfNotExist, buildFile, readTemplateFile } from '../utils';
 
-const ComponentName = 'Button';
-const config = JSON.parse(fs.readFileSync('rfsb.config.json', { encoding: 'utf8' }));
-const { name, componentEntryPoint, ...configRest } = config;
+// Get configs from rfsb.config.json file
+const { name, componentEntryPoint, ...configRest } = JSON.parse(fs.readFileSync('rfsb.config.json', { encoding: 'utf8' }));
+
+// Clean path
 const COMPONENTS_ROOT_DIR = sanitizePath(componentEntryPoint);
+const ComponentName = 'Button';
 
+// For each key create a file dynamically filled with template file's content
 for (const [key, aConfig] of Object.entries(sanitizeConfigPaths(configRest, ComponentName))) {
   const RELATIVE_PATH = aConfig.path || `${COMPONENTS_ROOT_DIR}/${ComponentName}/`;
-  if (key === 'component') {
-    createDirIfNotExist(RELATIVE_PATH);
-    const COMPONENT_DIR = configRest.style.path ? `${configRest.style.path}/` : '';
-    const FullStyleFileName = ComponentName + configRest.style.suffixExtension;
-    const STYLESHEET_IMPORT_PATH = `./${COMPONENT_DIR}${FullStyleFileName};`
-    const ComponentExport = aConfig.export === 'module'
-              ? `{ ${ComponentName} }`
-              : `default ${ComponentName}`;
-    const StyleImport = config.style.import === 'module' 
-              ? `import styles from '${STYLESHEET_IMPORT_PATH}'` 
-              : `import '${STYLESHEET_IMPORT_PATH}'`;
-    const template = JSON.stringify(fs.readFileSync(`./templates/${key}.template.ts`, { encoding: 'utf8' }));
-    const formatedTemplate = template
-      .replaceAll(ComponentNameTemplate, ComponentName)
-      .replaceAll(ComponentExportTemplate, ComponentExport)
-      .replaceAll(StyleImportTemplate, StyleImport);
+  createDirIfNotExist(RELATIVE_PATH);
 
+  if (key === 'component') {
+    const fullStyleFileName = ComponentName + configRest.style.suffixExtension;
+    const componentExportString = aConfig.export === 'module' ? `{ ${ComponentName} }` : `default ${ComponentName}`;
+    const STYLESHEET_DIRECTORY = configRest.style.path ? `${configRest.style.path}/` : '';
+    const STYLESHEET_IMPORT_PATH = `./${STYLESHEET_DIRECTORY}${fullStyleFileName};`
+    const styleImportString = configRest.style.import === 'module' ? `import styles from '${STYLESHEET_IMPORT_PATH}'` : `import '${STYLESHEET_IMPORT_PATH}'`;
+
+    // Dynamically replace template's tags with the right content
+    const formatedTemplate = JSON.stringify(readTemplateFile(key))
+      .replaceAll(ComponentNameTemplate, ComponentName)
+      .replaceAll(ComponentExportTemplate, componentExportString)
+      .replaceAll(StyleImportTemplate, styleImportString);
+
+    // Build component file
     buildFile(`${RELATIVE_PATH}/${ComponentName}${aConfig.suffixExtension}`, JSON.parse(formatedTemplate));
   }
   if (key === 'test') {
-    createDirIfNotExist(RELATIVE_PATH);
-    const COMPONENT_DIR = configRest.component.path ? `${configRest.component.path}/` : '';
     const fullComponentName = `${ComponentName}${configRest.component.suffixExtension}`;
+    const COMPONENT_DIR = configRest.component.path ? `${configRest.component.path}/` : '';
     const COMPONENT_IMPORT_PATH = `./${COMPONENT_DIR}${fullComponentName}`;
-    const FULL_COMPONENT_FILE_EXPORT = `import ${configRest.component.export === 'default' ? ComponentName : `{ ${ComponentName} }`} from '${COMPONENT_IMPORT_PATH}'`
+    const FULL_COMPONENT_FILE_IMPORT_STRING = `import ${configRest.component.export === 'default' ? ComponentName : `{ ${ComponentName} }`} from '${COMPONENT_IMPORT_PATH}'`
 
-    const template = JSON.stringify(fs.readFileSync(`./templates/${key}.template.ts`, { encoding: 'utf8' }));
-    const formatedTemplate = template
+    // Dynamically replace template's tags with the right content
+    const formatedTemplate = JSON.stringify(readTemplateFile(key))
       .replaceAll(ComponentNameTemplate, ComponentName)
-      .replaceAll(ComponentImportTemplate, FULL_COMPONENT_FILE_EXPORT);
+      .replaceAll(ComponentImportTemplate, FULL_COMPONENT_FILE_IMPORT_STRING);
 
+    // Build test file
     buildFile(`${RELATIVE_PATH}/${ComponentName}${aConfig.suffixExtension}`, JSON.parse(formatedTemplate));
   }
-  if(key === 'props') {
-    createDirIfNotExist(RELATIVE_PATH);
-    const template = JSON.stringify(fs.readFileSync(`./templates/${key}.template.ts`, { encoding: 'utf8' }));
-    const formatedTemplate = template
+  if (key === 'props') {
+    // Dynamically replace template's tags with the right content
+    const formatedTemplate = JSON.stringify(readTemplateFile(key))
       .replaceAll(ComponentNameTemplate, ComponentName);
 
+    // Build props file
     buildFile(`${COMPONENTS_ROOT_DIR}/${ComponentName}/${ComponentName}${aConfig.suffixExtension}`, JSON.parse(formatedTemplate));
   }
-  if(key === 'style') {
-    createDirIfNotExist(RELATIVE_PATH);
+  if (key === 'style') {
+    // Build style sheet
     buildFile(`${RELATIVE_PATH}/${ComponentName}${aConfig.suffixExtension}`, '');
   }
 }
