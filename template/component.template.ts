18c4dcdfa57acedc7caa01e7701c80a9afdd0ce4
amine-abdelli <amine.abdelli@outlook.fr>
2022-11-24 14:05:03 +0100

    feat(template): Create file templates

---
+++ template/component.template.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+<%style_import%>;
+
+const <%component_name%> = () => {
+  return (
+    <div><%component_name%></div>
+  )
+}
+
+export <%component_export%>;
---
+++ template/props.template.ts
@@ -0,0 +1,3 @@
+export interface <%component_name%>Props {
+
+}
