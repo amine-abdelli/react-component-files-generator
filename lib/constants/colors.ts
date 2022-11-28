5899b19d1db0722dd055f7e86b0cae95a8ac5c94
amine-abdelli <amine.abdelli@outlook.fr>
2022-11-28 15:59:33 +0100

    feat(constants): Migrate constants to lib folder

---
+++ lib/constants/colors.ts
@@ -0,0 +1,6 @@
+export enum Colors {
+  ERROR = '#ef5350',
+  SUCCESS = '#4caf50',
+  WARNING = 'ff9800',
+  INFO = '#000000'
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export * from './regex';
+export * from './template';
+export * from './colors';
