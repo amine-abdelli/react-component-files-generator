937ede93e72effddaa84c4a0fcabda5228889020
amine-abdelli <amine.abdelli@outlook.fr>
2022-11-28 15:55:22 +0100

    feat(help.ts): Create cli helper

---
+++ lib/help/help.ts
@@ -0,0 +1,8 @@
+console.log(`
+  Basic options:
+    rsfb --help / -h             Show help.
+    rsfb --init / -i             Run config initialization and creates a rfsb.config.json file.
+    rsfb --version / -v          Output the version number.
+    rsfb --build / -b            Build component.
+    rsfb --update / -u           Update config.
+`)
